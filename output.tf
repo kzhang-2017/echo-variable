fd436d6683bc4b8d9fe4ce83c0d33ed7fbf107f0
Kevin Zhang <116036437+kzhang-2017@users.noreply.github.com>
2025-06-19 22:18:13 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "template_file_1" {
-    value = data.template_file_1.test1.rendered
+    value = data.template_file.test1.rendered
 }
 
 output "template_file_2" {
-    value = data.template_file_2.test2.rendered
+    value = data.template_file.test2.rendered
 }
